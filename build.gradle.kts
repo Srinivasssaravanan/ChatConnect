cc2d8eaebda2b1310d871416a9b96ad6c756cf2a
Srinivasssaravanan <srinivass.s102004@gmail.com>
2024-11-20 23:15:24 +0530

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("com.android.library") version "8.5.2" apply false // Use the same AGP version for all plugins
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}

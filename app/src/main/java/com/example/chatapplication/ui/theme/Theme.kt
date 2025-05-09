e20c4dacc434a851c914823a1bc26f98a35fff42
Srinivasssaravanan <137177741+Srinivasssaravanan@users.noreply.github.com>
2025-05-10 01:23:39 +0530

    Update Theme.kt

--- app/src/main/java/com/example/chatapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/chatapplication/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.project.pradyotprakash.flashchat.ui.theme
+package com.project.flashchat.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
